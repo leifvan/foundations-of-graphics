974a78227a05036a0374d5019459135c1d59e8d9
Leif Van Holland <holland@uni-bonn.de>
2019-10-21 17:57:07 +0200

    add solutions for sheet 2 practical assignment

--- Sheet2/LorenzAttractor.py
+++ Sheet2/LorenzAttractor.py
@@ -3,48 +3,69 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.integrate import odeint
 
+SIGMA = 10
+RHO = 28
+BETA = 8 / 3
+
 
 def LorenzAttractor(t, X):
-	"""
-	Computes the force at a time t and a position X
-	t: the current time. In this case, the function is constant in t
-	X: the position as [x, y, z] array
-	"""
-	x = np.zeros((3))
-	# your code goes here...
-	return x
+    """
+    Computes the force at a time t and a position X
+    t: the current time. In this case, the function is constant in t
+    X: the position as [x, y, z] array
+    """
+    x = np.zeros((3,))
+    # your code goes here...
+    x[0] = SIGMA * (X[1] - X[0])
+    x[1] = X[0] * (RHO - X[2]) - X[1]
+    x[2] = X[0] * X[1] - BETA * X[2]
+    return x
+
 
 def Euler(f, y0, stepsize, steps):
-	"""
-	Compute positions using the Euler integration scheme.
-	f:			The function that is being integrated
-	y0:			The start position
-	stepsize:	Delta for each step
-	steps:		Amount of integration steps
-	"""
-	x = np.zeros((3, steps))
-	# your code goes here...
-	return x
+    """
+    Compute positions using the Euler integration scheme.
+    f:			The function that is being integrated
+    y0:			The start position
+    stepsize:	Delta for each step
+    steps:		Amount of integration steps
+    """
+    x = np.zeros((3, steps))
+    # your code goes here...
+    yi = y0
+    for i in range(steps):
+        yi += stepsize * f(0, yi)
+        x[:, i] = yi
+    return x
+
 
 def RungeKutta4thOrder(f, y0, stepsize, steps):
-	"""
-	Compute positions using the Runge-Kutta 4th Order integration scheme.
-	f:			The function that is being integrated
-	y0:			The start position
-	stepsize:	Delta for each step
-	steps:		Amount of integration steps
-	"""
-	x = np.zeros((3, steps))
-	# your code goes here...
-	return x
+    """
+    Compute positions using the Runge-Kutta 4th Order integration scheme.
+    f:			The function that is being integrated
+    y0:			The start position
+    stepsize:	Delta for each step
+    steps:		Amount of integration steps
+    """
+    x = np.zeros((3, steps))
+    # your code goes here...
+    yi = y0
+    k = np.zeros((4, 3))
+    for i in range(steps):
+        k[0] = f(0, yi)
+        k[1] = 2 * f(0, yi + 0.5 * stepsize * k[0])
+        k[2] = 2 * f(0, yi + 0.5 * stepsize * k[1])
+        k[3] = f(0, yi + stepsize * k[2])
+        yi += stepsize * (1 / 6) * k.sum(axis=0)
+        x[:, i] = yi
+    return x
 
-	
+
 # Compute Values
 y0 = [-1, 3, 4]
 euler = Euler(LorenzAttractor, y0, 0.025, 3000)
 rungekutta = RungeKutta4thOrder(LorenzAttractor, y0, 0.025, 3000)
-ref = odeint(lambda y, t : LorenzAttractor(t, y), y0, np.arange(0, 0.025*3000, 0.025)).T
-
+ref = odeint(lambda y, t: LorenzAttractor(t, y), y0, np.arange(0, 0.025 * 3000, 0.025)).T
 
 # Plot Everything
 plt.figure()

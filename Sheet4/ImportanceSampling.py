819c4f7049339cfe327a41d770eb718d5901ea2f
Leif Van Holland <holland@uni-bonn.de>
2019-11-13 19:41:55 +0100

    add solution for importance sampling task

--- Sheet4/ImportanceSampling.py
+++ Sheet4/ImportanceSampling.py
@@ -1,25 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from math import exp, sqrt, pi
+from scipy.signal import savgol_filter
 
 
 # f: target function, which we want to integrate
-# ...
+def f(x):
+    return x ** 0.9 * np.exp(-x ** 2 / 2)
 
 
 # p_n: normal distribution for samples
 sigma2 = 0.3
 mu = 1.2
-# ...
+
+
+def p_n(x):
+    return 1 / (np.sqrt(2 * np.pi * sigma2)) * np.exp(-(mu - x) ** 2 / (2 * sigma2))
+
 
 # p_p: polynomial fit of the target function
-# ...
-# (make sure, the fitting is computed only once and not on every invocation of p_p !
+fit_x = np.arange(0, 5)
+fit_y = f(fit_x)
+p_p = np.polynomial.Polynomial.fit(fit_x, fit_y, deg=4, domain=[0, 4])
 
 # plot the function graphs:
 plt.figure("Functions")
-# ...
-
+plot_x = np.linspace(0, 4, 80)
+plt.plot(plot_x, f(plot_x), label="$f$")
+plt.plot(plot_x, p_n(plot_x), label="$p_n$")
+plt.plot(plot_x, p_p(plot_x), label="$p_p$")
+plt.legend()
+plt.show()
 
 """
 Uses rejection sampling to generate samples
@@ -28,30 +38,46 @@ d: distribution to draw samples from
 max: maximum y value that is used to generate the samples
 returns: x and y values of the samples in the shape (n, 2)
 """
+
+
 def GenSamples(n, d, max):
-	s = np.zeros((n, 2))
-	# ...
-	return s
+    s = np.zeros((n, 2))
+    num_accepted = 0
+    while num_accepted < n:
+        points = np.random.random(size=(n - num_accepted, 2))
+        points[:, 0] *= 4
+        points[:, 1] *= max
+        dist_vals = d(points[:, 0])
+        points = points[points[:, 1] <= dist_vals]
+        s[num_accepted:num_accepted + len(points)] = points
+        num_accepted += len(points)
+    return s
 
 
 # Plot results of GenSamples()
 # Hint: 0.8 is a reasonable value for the max parameter of GenSamples
 plt.figure("Normal Distribution")
-# ...
+s = GenSamples(200, p_n, 0.8)
+plt.plot(plot_x, p_n(plot_x), c="C1")
+plt.scatter(s[:, 0], s[:, 1], s=3, c="C1")
+plt.show()
 
 plt.figure("Polynomial")
-# ...
-
+s = GenSamples(200, p_p, 0.8)
+plt.plot(plot_x, p_p(plot_x), c="C2")
+plt.scatter(s[:, 0], s[:, 1], s=3, c="C2")
+plt.show()
 
 """
 p: the function to integrate
 samples: array with the sample positions
 weights: function to compute the weight of each sample
 """
-def Integrate(p, samples, weights):
-	# ...
-	pass
 
+
+def Integrate(p, samples, weights):
+    weighted_samples = p(samples) / weights(samples)
+    return weighted_samples.mean()
 
 
 maximumSamples = 500
@@ -59,13 +85,23 @@ id = np.zeros(maximumSamples)
 norm = np.zeros(maximumSamples)
 poly = np.zeros(maximumSamples)
 
-for i in range(maximumSamples):
-	# id[i] =
-	# norm[i] =
-	# poly[i] =
-	if i%10 == 0: # print progress
-		print(i)
+for i in range(1, maximumSamples):
+    id[i] = Integrate(f, GenSamples(i, lambda x: 1, 1)[:, 0], lambda x: 1/4)
+    norm[i] = Integrate(f, GenSamples(i, p_n, 0.8)[:, 0], p_n)
+    poly[i] = Integrate(f, GenSamples(i, p_p, 0.8)[:, 0], p_p)
+    if i % 10 == 0:  # print progress
+        print(i)
 
-#plot results
+# try to numerically integrate via trapezoidal method
+trap_x = np.linspace(0, 4, 10*maximumSamples)
+real_val = np.trapz(f(trap_x), trap_x)
+
+# plot results
 plt.figure("Convergence")
-# ...
+plt.plot([0,maximumSamples],[real_val, real_val], label="correct", c="black")
+# smooth output with Savitzky-Golay filter
+plt.plot(savgol_filter(id,11,3), label="uniform", alpha=0.8, lw=1)
+plt.plot(savgol_filter(norm,11,3), label="normal", alpha=0.8, lw=1)
+plt.plot(savgol_filter(poly,11,3), label="polynomial", alpha=0.8, lw=1)
+plt.legend()
+plt.show()

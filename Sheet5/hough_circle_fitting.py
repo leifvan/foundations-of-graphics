714cf9d3583685887ffdf23b68590cf2a609ac7e
Rechargeable22 <oliver.leuschner@gmx.de>
2019-11-19 13:46:50 +0100

    Solved ransac_circle

--- Sheet5/hough_circle_fitting.py
+++ Sheet5/hough_circle_fitting.py
@@ -17,55 +17,57 @@ import scipy.ndimage as ndim
 from skimage.transform import hough_circle
 from scipy.stats import multivariate_normal
 
+
 # Implement a function that, given an edge image and a range of radii, performs the Circular Hough Transform to create an accumulation array containing the votes for circles centered at the specific pixel and with a specific radius.
 def hough_circles(edge_map, radius_range):
-	# you can compare you solution, to a reference implementation:
-	acc = hough_circle(edge_map, radius_range) # <- remove this
+    # you can compare you solution, to a reference implementation:
+    acc = hough_circle(edge_map, radius_range)  # <- remove this
 
-	# TODO: remove above line and implement your own Hough Transformation:
-	# acc = ...
-	return acc
+    # TODO: remove above line and implement your own Hough Transformation:
+    # acc = ...
+    return acc
+
 
 # Implement this function for displaying four different slices for specific radii from the accumulation array. The first argument is the Hough accumulation array, the second argument is the same range of radii that was used to create the acculuation array.
 def display_acc_slices(acc, radius_range):
-	# create figure and axes inside of this figure
-	(fh, ((ax1, ax2), (ax3, ax4), (ax5, ax6))) = plt.subplots(nrows=3, ncols=2)
-	# display four slices as images using imshow
-	# - read up on the vmin and vmax parameters to set the same color scaling for each slice
-	# - set a title for each sub plot that shows the corresponding radius
+    # create figure and axes inside of this figure
+    (fh, ((ax1, ax2), (ax3, ax4), (ax5, ax6))) = plt.subplots(nrows=3, ncols=2)
+    # display four slices as images using imshow
+    # - read up on the vmin and vmax parameters to set the same color scaling for each slice
+    # - set a title for each sub plot that shows the corresponding radius
 
-	fh.suptitle("Radii slices")
-	idx = np.linspace(0, acc.shape[0]-1, 6, dtype=np.int32)
-	
-	ax1.imshow(acc[idx[0],:,:], vmin=0, vmax=0.5)
-	ax2.imshow(acc[idx[1],:,:], vmin=0, vmax=0.5)
-	ax3.imshow(acc[idx[2],:,:], vmin=0, vmax=0.5)
-	ax4.imshow(acc[idx[3],:,:], vmin=0, vmax=0.5)
-	ax5.imshow(acc[idx[4],:,:], vmin=0, vmax=0.5)
-	ax6.imshow(acc[idx[5],:,:], vmin=0, vmax=0.5)
+    fh.suptitle("Radii slices")
+    idx = np.linspace(0, acc.shape[0] - 1, 6, dtype=np.int32)
+
+    ax1.imshow(acc[idx[0], :, :], vmin=0, vmax=0.5)
+    ax2.imshow(acc[idx[1], :, :], vmin=0, vmax=0.5)
+    ax3.imshow(acc[idx[2], :, :], vmin=0, vmax=0.5)
+    ax4.imshow(acc[idx[3], :, :], vmin=0, vmax=0.5)
+    ax5.imshow(acc[idx[4], :, :], vmin=0, vmax=0.5)
+    ax6.imshow(acc[idx[5], :, :], vmin=0, vmax=0.5)
 
 
 # displays a set of circles, specified by three arrays containing x- & y-coordinates, as well as circle radius; optionally a score can be specified that is visulalized via the circle color (brighter color == higher score)
 def draw_circle(axes_handle, centers_x, centers_y, radii, scores=None):
-	axes_handle.hold(True)
-	n = 100 # number of samples for drawing circles
-	if scores is None:
-		colors = np.repeat(np.reshape([0, 0, 1], (1, 3)), 10, axis=0)
-	else:
-		(smin, smax) = (np.min(scores), np.max(scores))
-		# create color map
-		colors = cm.hot(np.arange(0, 256))
-		colors = colors[63 : 192, 0:3]
-		# use scores as indices into colormap	
-		scores = np.int64(np.ceil(127. * ((scores - smin) / (smax - smin))))
-		colors = colors[scores, :]
-	
-	# iterate over all circles that are passed
-	for x, y, radius, color in zip(centers_x, centers_y, radii, colors):
-		angles = np.arange(0, (2. - 2. / n) * pi, 1. / n)
-		(xs, ys) = (radius * np.cos(angles) + x, radius * np.sin(angles) + y)
-		axes_handle.plot(xs, ys, color=tuple(color), linewidth=5)
-	fh.canvas.draw()
+    axes_handle.hold(True)
+    n = 100  # number of samples for drawing circles
+    if scores is None:
+        colors = np.repeat(np.reshape([0, 0, 1], (1, 3)), 10, axis=0)
+    else:
+        (smin, smax) = (np.min(scores), np.max(scores))
+        # create color map
+        colors = cm.hot(np.arange(0, 256))
+        colors = colors[63: 192, 0:3]
+        # use scores as indices into colormap
+        scores = np.int64(np.ceil(127. * ((scores - smin) / (smax - smin))))
+        colors = colors[scores, :]
+
+    # iterate over all circles that are passed
+    for x, y, radius, color in zip(centers_x, centers_y, radii, colors):
+        angles = np.arange(0, (2. - 2. / n) * pi, 1. / n)
+        (xs, ys) = (radius * np.cos(angles) + x, radius * np.sin(angles) + y)
+        axes_handle.plot(xs, ys, color=tuple(color), linewidth=5)
+    fh.canvas.draw()
 
 
 # read image & compute edge map
@@ -73,10 +75,10 @@ img = io.imread("coins.png")
 
 # convert to gray image if necessary
 if len(img.shape) > 2:
-	img = img[:, :, 0]
+    img = img[:, :, 0]
 
 # edge detection
-#em = feature.canny(img)
+# em = feature.canny(img)
 em = sobel(img)
 cutoff = np.sqrt(4 * np.mean(em))
 em = feature.canny(em, high_threshold=cutoff)
@@ -117,7 +119,7 @@ scores = []
 
 # perform slight smoothing in hough space
 acc = ndim.filters.gaussian_filter(acc, sigma=0.5)
-#display_acc_slices(acc, hough_radii)
+# display_acc_slices(acc, hough_radii)
 
 # find local maxima
 # we subtract a 3D-gaussian centered around each local maximum to avoid extracting similar circles from nearby bins (not mandatory for solution)
@@ -127,26 +129,26 @@ grid[:, :, :, 0] = rs
 grid[:, :, :, 1] = xs
 grid[:, :, :, 2] = ys
 for ii in np.arange(0, num_circles):
-	# find current maximum
-	(rad, y, x) = np.unravel_index(np.argmax(acc), acc.shape)
-	# extract circle parameters corresponding to this maximum
-	scores.extend([acc[rad, y, x]])
-	centers.extend(np.array([x, y]).reshape(1, 2))
-	radii.extend([hough_radii[rad]])
-	# not mandatory for solution
-	# we now want to get rid of this maximum to be able to find the next lower one
-	# set up and evaluate 3D gaussian
-	# uniform gaussian with standard deviation 0.1
-	covariance = np.eye(3) * 10
-	mvn = multivariate_normal([hough_radii[rad], y, x], covariance)
-	acc_sub = mvn.pdf(grid)
-	# scale gaussian	
-	acc_sub *= scores[-1] / np.max(acc_sub)
-	# subtract gaussian from accumulation array
-	acc -= acc_sub
-	# a much simpler and faster acceptable solution is setting the local maxima to 0
-	# this produces poor results, though
-	#acc[rad, y, x] = 0
+    # find current maximum
+    (rad, y, x) = np.unravel_index(np.argmax(acc), acc.shape)
+    # extract circle parameters corresponding to this maximum
+    scores.extend([acc[rad, y, x]])
+    centers.extend(np.array([x, y]).reshape(1, 2))
+    radii.extend([hough_radii[rad]])
+    # not mandatory for solution
+    # we now want to get rid of this maximum to be able to find the next lower one
+    # set up and evaluate 3D gaussian
+    # uniform gaussian with standard deviation 0.1
+    covariance = np.eye(3) * 10
+    mvn = multivariate_normal([hough_radii[rad], y, x], covariance)
+    acc_sub = mvn.pdf(grid)
+    # scale gaussian
+    acc_sub *= scores[-1] / np.max(acc_sub)
+    # subtract gaussian from accumulation array
+    acc -= acc_sub
+# a much simpler and faster acceptable solution is setting the local maxima to 0
+# this produces poor results, though
+# acc[rad, y, x] = 0
 
 
 centers = np.array(centers)
@@ -158,7 +160,6 @@ idxs = np.argsort(scores)
 centers = centers[idxs[::-1], :]
 radii = radii[idxs[::-1]]
 scores = scores[idxs[::-1]]
-
 
 # display results
 draw_circle(ax1, centers[:, 0], centers[:, 1], radii, scores)
